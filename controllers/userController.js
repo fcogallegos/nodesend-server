6e5f01c298a983020215cd808b046ea90eac6b48
Francisco Gallegos <fgallegosgo@gmail.com>
2020-08-19 12:59:40 -0600

    how avoid duplicates registers

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,16 @@ const User = require('../models/User');
 exports.newUser = async (req, res) => {
 
 
-    //console.log(req.body);
+    //verify if the user already exist
+    const { email } = req.body;
 
-    const user = await new User(req.body);
+    let user = await User.findOne({ email });
+
+    if(user) {
+        return res.status(400).json({ msg: 'The user already exist' });
+    }
+
+    user = await new User(req.body);
     user.save();
 
     res.json({msg: 'User created successfully'});
