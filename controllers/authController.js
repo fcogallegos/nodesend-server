2fbe33260f8f18dfddb82044fe5e9dce71886e42
Francisco Gallegos <fgallegosgo@gmail.com>
2020-08-20 13:21:45 -0600

    cheking if the user exists or no

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,24 @@
+const User = require('../models/User');
 
 
 exports.authenticateUser = async (req, res, next) => {
 
+    //check if there are errors
+
+    //search the user for see if is registered
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    console.log(user);
+
+    if(!user) {
+        res.status(401).json({ msg: 'The User is not exist' });
+        return next(); //stop the execution
+    }
+
+    //verify the password and authenticate the user
+    console.log('The User if exists');
 }
 
 exports.userAuthenticated = async (req, res) => {
-    
+
 } 
