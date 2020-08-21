bc5a2a77e5f89291bd6f9d72e12ba259a1348edb
Francisco Gallegos <fgallegosgo@gmail.com>
2020-08-21 14:23:14 -0600

    creating the routing and controllers of files

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ const port = process.env.PORT || 12000;
 //enable to read the data of a body
 app.use( express.json() );
 
-//routes of the app
+//routes of the app - endpoints
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/links', require('./routes/links'));
+app.use('/api/files', require('./routes/files'));
 
 // run the app
 app.listen(port, '0.0.0.0', () => {
---
+++ routes/files.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const filesController = require('../controllers/filesController');
+const auth = require('../middleware/auth');
+
+
+router.post('/',
+    filesController.uploadFile
+);
+
+router.delete('/:id',
+    filesController.deleteFile
+);
+
+
+module.exports = router;
